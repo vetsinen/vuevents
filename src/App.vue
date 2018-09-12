212ba0c45d9340497cc7b73485c8ea2e691c08d8
vhodzevych <vhodzevych@n-ix.com>
2018-09-12 22:50:32 +0300

    lists events

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Wielcomen to Your Vue.js App"/>
+    <Events msg="Wielcomen to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Events from './components/Events.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Events
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Events.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="events">
+        <ul>
+            <li v-for="event in events">{{event}}</li>
+        </ul>
+        <button v-on:click="next">next</button>
+        <footer>
+            footer: For guide and {{ msg }} on how to
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'events',
+        props: {
+            msg: String
+        },
+        data: function () {
+            return {
+                events: ['party', 'conference', 'concert']
+            }
+        },
+        methods: {
+            next: function () {
+                console.log('ok');
+                this.evbar();
+                this.readTextFile("file:////home/vhodzevych/test.txt");
+            },
+            evbar: function(){},
+            readTextFile: function (file) {
+                var rawFile = new XMLHttpRequest();
+                rawFile.open("GET", file, false);
+                rawFile.onreadystatechange = function () {
+                    if (rawFile.readyState === 4) {
+                        if (rawFile.status === 200 || rawFile.status == 0) {
+                            var allText = rawFile.responseText;
+                            alert(allText);
+                        }
+                    }
+                };
+                rawFile.send(null);
+            }
+
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
